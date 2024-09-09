ecbb5db9ffda412b404d25b32e6265dd8c36cf99
Minimal-Alexi <alex.pop@metropolia.fi>
2024-09-09 13:51:32 +0300

    Refractoring tours API to MVC pattern.

--- Week_4/MondayPair/app.js
+++ Week_4/MondayPair/app.js
@@ -1,35 +1,14 @@
 const express = require("express");
 const app = express();
-
-const {
-  getAllTours,
-  getTourById,
-  createTour,
-  updateTour,
-  deleteTour,
-} = require("./tourHandlers.js"); 
+const tourRouter = require ('./routers/tourRouter')
 
 // Middleware to parse JSON
 app.use(express.json());
 
-// ROUTES
+app.use("/", tourRouter)
 
-// GET /tours
-app.get("/tours", getAllTours);
+const port = 4000
 
-// POST /tours
-app.post("/tours", createTour);
-
-// GET /tours/:tourId
-app.get("/tours/:tourId", getTourById);
-
-// PUT /tours/:tourId
-app.put("/tours/:tourId", updateTour);
-
-// DELETE /tours/:tourId
-app.delete("/tours/:tourId", deleteTour);
-
-const port = 4000;
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
